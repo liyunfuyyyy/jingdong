23214032b209498539421e574a06210b18adee3d
luniont <1511491804@qq.com>
2022-03-07 13:30:47 +0800

    测试vercel部署跨域

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,70 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+const routes = [{
+  path: '/',
+  name: 'Home',
+  component: () => import(/* webpackChunkName: "home" */ '../views/home/Home')
+}, {
+  path: '/cartList',
+  name: 'CartList',
+  component: () => import(/* webpackChunkName: "cartList" */ '../views/cartList/CartList')
+}, {
+  path: '/orderConfirmation/:id/:addressId?',
+  name: 'OrderConfirmation',
+  component: () => import(/* webpackChunkName: "orderConfirmation" */ '../views/orderConfirmation/OrderConfirmation')
+}, {
+  path: '/orderList',
+  name: 'OrderList',
+  component: () => import(/* webpackChunkName: "orderList" */ '../views/orderList/OrderList')
+}, {
+  path: '/shop/:id',
+  name: 'Shop',
+  component: () => import(/* webpackChunkName: "shop" */ '../views/shop/Shop')
+}, {
+  path: '/register',
+  name: 'Register',
+  component: () => import(/* webpackChunkName: "register" */ '../views/register/Register'),
+  beforeEnter(to, from, next) {
+    const { isLogin } = localStorage
+    isLogin ? next({ name: 'Home' }) : next()
   }
+}, {
+  path: '/login',
+  name: 'Login',
+  component: () => import(/* webpackChunkName: "login" */ '../views/login/Login'),
+  beforeEnter(to, from, next) {
+    const { isLogin } = localStorage
+    isLogin ? next({ name: 'Home' }) : next()
+  }
+}, {
+  path: '/chooseAddressList/:shopId',
+  name: 'ChooseAddressList',
+  component: () => import(/* webpackChunkName: "chooseAddressList" */ '../views/chooseAddressList/ChooseAddressList')
+}, {
+  path: '/myAddressList',
+  name: 'MyAddressList',
+  component: () => import(/* webpackChunkName: "myAddressList" */ '../views/myAddressList/MyAddressList')
+}, {
+  path: '/upsertAddress/:id?',
+  name: 'UpsertAddress',
+  component: () => import(/* webpackChunkName: "upsertAddress" */ '../views/upsertAddress/UpsertAddress')
+}, {
+  path: '/person',
+  name: 'PersonalInfo',
+  component: () => import(/* webpackChunkName: "person" */ '../views/personalInfo/PersonalInfo')
+}
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const { isLogin } = localStorage
+  const { name } = to
+  const isLoginOrRegister = (name === 'Login' || name === 'Register');
+  (isLogin || isLoginOrRegister) ? next() : next({ name: 'Login' })
 })
 
 export default router
---
+++ src/utils/request.js
@@ -0,0 +1,45 @@
+import axios from 'axios'
+
+const instance = axios.create({
+  baseURL: 'https://jadpi.liyunfu.xyz',
+  timeout: 10000,
+  withCredentials: true
+})
+
+export const get = (url, params = {}) => {
+  return new Promise((resolve, reject) => {
+    instance.get(url, { params }).then((res) => {
+      resolve(res.data)
+    }, err => {
+      reject(err)
+    })
+  })
+}
+
+export const post = (url, data = {}) => {
+  return new Promise((resolve, reject) => {
+    instance.post(url, data, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then((res) => {
+      resolve(res.data)
+    }, err => {
+      reject(err)
+    })
+  })
+}
+
+export const patch = (url, data = {}) => {
+  return new Promise((resolve, reject) => {
+    instance.patch(url, data, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => {
+      resolve(res.data)
+    }, err => {
+      reject(err)
+    })
+  })
+}
